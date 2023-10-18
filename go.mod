f48522b9b28b3fe0f2cfe5bf3e7b7d3efede1ba0
cheikh2shift <cheeikhseck@gmail.com>
2023-10-18 14:52:06 +0000

    add example for rate package

--- go.mod
+++ go.mod
@@ -6,8 +6,8 @@ require (
 	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.9.1
 	github.com/mattn/go-sqlite3 v1.14.17
-	github.com/rs/cors v1.10.1
 	golang.org/x/text v0.9.0
+	golang.org/x/time v0.3.0
 )
 
 require (
---
+++ rate/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"golang.org/x/time/rate"
+)
+
+func main() {
+
+	limiter := rate.NewLimiter(10, 5)
+	ctx, _ := context.WithDeadline(
+		context.Background(),
+		time.Now().Add(10*time.Second),
+	)
+	for i := 0; i < 100; i++ {
+
+		go func(i int) {
+
+			log.Printf(
+				"ETA %v to process %v\n",
+				limiter.Reserve().Delay(),
+				i,
+			)
+
+			err := limiter.Wait(ctx)
+
+			if err != nil {
+				log.Println("Error processing", i, err)
+				return
+			}
+
+			log.Println("n", i)
+			time.Sleep(2 * time.Second)
+
+		}(i)
+
+	}
+
+	time.Sleep(10 * time.Second)
+}
